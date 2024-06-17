c1e1ada5ebd04e5c7cbeaba3abdd5f14fa8d0927
KaroLLain <karolina.anna.knap@gmail.com>
2024-06-17 11:43:24 +0200

    commit

--- server/index.js
+++ server/index.js
@@ -16,12 +16,12 @@ app.use(function(req, res, next) {
      next();
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//   );
+// }
 
 app.use("/", (req, res)=>{
   res.send("Server is running")
