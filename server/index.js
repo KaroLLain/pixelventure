6064a55063e3d8e449c98e3910b292d8f4594a00
KaroLLain <karolina.anna.knap@gmail.com>
2024-08-30 09:12:56 +0200

    s

--- server/index.js
+++ server/index.js
@@ -1,30 +1,42 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const contactRoute = require("./contractRoute");
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const path = require("path");
+// const contactRoute = require("./contractRoute");
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.use("/", contactRoute);
+
+// app.use("/", (req, res)=>{
+//   res.send("Server is running")
+// })
+
+// const port = process.env.PORT || 5000;
+// app.listen(port, console.log(`server listing to port 5000 only`));
+
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+// const path = require('path');
+const contactRoute = require('./contractRoute');
 
 const app = express();
 
+app.use(cors()); // Allow all origins for testing purposes
 app.use(express.json());
-app.use(cors());
 
-app.use("/", contactRoute);
+app.use('/', contactRoute);
 
-app.use("/", (req, res)=>{
-  res.send("Server is running")
-})
+app.use('/', (req, res) => {
+res.send('Server is running');
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port, console.log(`server listing to port 5000 only`));
-
-// require('dotenv').config();
-// const express = require('express');
-// const cors = require('cors');
-// // const path = require('path');
-// const contactRoute = require('./contractRoute');
-
-// const app = express();
+app.listen(port, () => console.log(`server listening to port ${port}`));
 
 // const corsOptions = {
 //   origin: 'https://www.pixelventure.eu', // Your frontend's origin
