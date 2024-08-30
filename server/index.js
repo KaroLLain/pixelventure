a4e6fa46f343fdbb7ce95579556730b44d0fc004
KaroLLain <karolina.anna.knap@gmail.com>
2024-08-30 13:04:10 +0200

    s

--- server/index.js
+++ server/index.js
@@ -1,20 +1,48 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const contactRoute = require("./contractRoute");
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const path = require("path");
+// const contactRoute = require("./contractRoute");
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.use("/", contactRoute);
+
+// app.use("/", (req, res)=>{
+//   res.send("Server is running")
+// })
+
+// const port = process.env.PORT || 5000;
+// app.listen(port, console.log(`server listing to port 5000 only`));
+
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const contactRoute = require('./contactRoute');
 
 const app = express();
 
+// Configure CORS to allow requests from your client-side domain
+app.use(cors({
+origin: 'https://www.pixelventure.eu'
+}));
+
 app.use(express.json());
-app.use(cors());
 
-app.use("/", contactRoute);
+app.use('/', contactRoute);
 
-app.use("/", (req, res)=>{
-  res.send("Server is running")
-})
+app.use('/', (req, res) => {
+res.send('Server is running');
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port, console.log(`server listing to port 5000 only`));
+app.listen(port, () => console.log(`server listening to port ${port}`));
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+console.error(err.stack);
+res.status(500).send('Something broke!');
+});
