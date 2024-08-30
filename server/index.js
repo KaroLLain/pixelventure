523a5999b4c634f8cb5a5ed5271a29426c21d991
KaroLLain <karolina.anna.knap@gmail.com>
2024-08-30 12:49:24 +0200

    s

--- server/index.js
+++ server/index.js
@@ -1,76 +1,20 @@
-// require("dotenv").config();
-// const express = require("express");
-// const cors = require("cors");
-// const path = require("path");
-// const contactRoute = require("./contractRoute");
-
-// const app = express();
-
-// app.use(express.json());
-// app.use(cors());
-
-// app.use("/", contactRoute);
-
-// app.use("/", (req, res)=>{
-//   res.send("Server is running")
-// })
-
-// const port = process.env.PORT || 5000;
-// app.listen(port, console.log(`server listing to port 5000 only`));
-
-
-
-
-require('dotenv').config();
-const cors = require('cors');
-const express = require('express');
-const contactRoute = require('./contactRoute');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const contactRoute = require("./contractRoute");
 
 const app = express();
 
-// // Configure CORS to allow requests from your client-side domain
-// app.use(cors({
-// origin: 'https://www.pixelventure.eu'
-// }));
+app.use(express.json());
+app.use(cors());
 
-// app.use(express.json());
+app.use("/", contactRoute);
 
-// app.use('/', contactRoute);
+app.use("/", (req, res)=>{
+  res.send("Server is running")
+})
 
-// app.use('/', (req, res) => {
-// res.send('Server is running');
-// });
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`server listing to port 5000 only`));
 
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => console.log(`server listening to port ${port}`));
-
-
-
-
-
-const corsOptions = {
-  origin: 'https://www.pixelventure.eu', // Your frontend's origin
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204
-  };
-  
-  app.use(cors(corsOptions));
-  app.use(express.json());
-  
-  app.use('/', contactRoute);
-  
-  app.use('/', (req, res) => {
-  res.send('Server is running');
-  });
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://www.pixelventure.eu');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-    });
-  
-  const port = process.env.PORT || 5000;
-  app.listen(port, console.log(`server listening to port ${port}`));
-  app.options('*', cors(corsOptions)); // Enable pre-flight across-the-board
