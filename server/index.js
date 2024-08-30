ee5c374de6c49b7847257bedd58471dc3fbac146
KaroLLain <karolina.anna.knap@gmail.com>
2024-08-30 13:28:26 +0200

    s

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const corsOptions ={
+  origin:'https://www.pixelventure.eu', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
 const path = require("path");
 const contactRoute = require("./contractRoute");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/", contactRoute);
 
