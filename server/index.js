4eab8d86ab42d3c6f8ffa93eb6f745a01df90abf
KaroLLain <karolina.anna.knap@gmail.com>
2024-09-02 11:03:34 +0200

    a

--- server/index.js
+++ server/index.js
@@ -1,14 +1,41 @@
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const corsOptions ={
+//   origin:'https://www.pixelventure.eu', 
+//   credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200
+// }
+
+// const path = require("path");
+// const contactRoute = require("./contractRoute");
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors(corsOptions));
+
+// app.use("/", contactRoute);
+
+// app.use("/", (req, res)=>{
+//   res.send("Server is running")
+// })
+
+// const port = process.env.PORT || 5000;
+// app.listen(port, console.log(`server listing to port 5000 only`));
+
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const corsOptions ={
-  origin:'https://www.pixelventure.eu', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
+
+const corsOptions = {
+origin: 'https://www.pixelventure.eu',
+credentials: true, // access-control-allow-credentials:true
+optionSuccessStatus: 200
+};
 
 const path = require("path");
-const contactRoute = require("./contractRoute");
+const contactRoute = require("./contactRoute"); // Ensure this matches your file name
 
 const app = express();
 
@@ -17,10 +44,9 @@ app.use(cors(corsOptions));
 
 app.use("/", contactRoute);
 
-app.use("/", (req, res)=>{
-  res.send("Server is running")
-})
+app.use("/", (req, res) => {
+res.send("Server is running");
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port, console.log(`server listing to port 5000 only`));
-
+app.listen(port, () => console.log(`Server listening on port ${port}`));
