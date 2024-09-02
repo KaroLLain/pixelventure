d687f43f458e7071d2eaabd656087e35c1dac714
KaroLLain <karolina.anna.knap@gmail.com>
2024-09-02 12:38:05 +0200

    a

--- server/index.js
+++ server/index.js
@@ -1,33 +1,3 @@
-// require("dotenv").config();
-// const express = require("express");
-// const cors = require("cors");
-
-// const corsOptions ={
-//   origin:'https://www.pixelventure.eu', 
-//   credentials:true,            //access-control-allow-credentials:true
-//   optionSuccessStatus:200
-// }
-
-// const path = require("path");
-// const contactRoute = require("./contactRoute");
-
-// const app = express();
-
-// app.use(express.json());
-// app.use(cors(corsOptions));
-
-// app.use("/", contactRoute);
-
-// app.use("/", (req, res)=>{
-//   res.send("Server is running")
-// })
-
-// const port = process.env.PORT || 5000;
-// app.listen(port, console.log(`server listing to port 5000 only`));
-
-
-
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
